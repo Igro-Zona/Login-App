491b25b6a424357c80379919b593aebb22ce3df6
Igro-Zona <tim.bic22@gmail.com>
2025-06-15 01:57:21 +0300

    build!: refactored the code to implement my own project

--- src/main/java/com/login/LoginController.java
+++ src/main/java/com/login/LoginController.java
@@ -3,10 +3,9 @@ package com.login;
 import java.io.IOException;
 import javafx.fxml.FXML;
 
-public class PrimaryController {
+public class LoginController {
 
     @FXML
     private void switchToSecondary() throws IOException {
-        App.setRoot("secondary");
     }
 }
