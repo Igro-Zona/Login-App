811496b6977ec2967982488cd08fcd2fd30a5431
Igro-Zona <tim.bic22@gmail.com>
2025-06-15 03:11:57 +0300

    feat(ui): created a prototyped user interface for the app

--- src/main/java/com/login/App.java
+++ src/main/java/com/login/App.java
@@ -14,7 +14,7 @@ public class App extends Application {
 
     @Override
     public void start(Stage stage) throws IOException {
-        scene = new Scene(loadFXML("login"), 640, 480);
+        scene = new Scene(loadFXML("login"));
         stage.setScene(scene);
         stage.show();
     }
