a027c9a8e378f85cbcb92de253d8d8b2c63e7c34
Igro-Zona <tim.bic22@gmail.com>
2025-06-19 02:22:31 +0300

    feat!: documented the logic for future ui development

--- src/main/java/com/login/LoginController.java
+++ src/main/java/com/login/LoginController.java
@@ -11,36 +11,37 @@ import javafx.scene.control.TextField;
 import javafx.stage.Stage;
 
 public class LoginController {
-    @FXML
-    private Label message;
+    // @FXML
+    // private Label message;
 
-    @FXML
-    private PasswordField passwordInput;
+    // @FXML
+    // private PasswordField passwordInput;
 
-    @FXML
-    private TextField usernameInput;
+    // @FXML
+    // private TextField usernameInput;
 
-    @FXML
-    private Button button1;
+    // @FXML
+    // private Button button1;
 
-    @FXML
-    private Button button2;
+    // @FXML
+    // private Button button2;
 
-    public void initialize() {
-        message.setText("Enter your informaion:");
-    }
+    // public void initialize() {
+    // message.setText("Enter your informaion:");
+    // }
 
-    public void button2OnAction(ActionEvent e) throws IOException {
-        Stage stage = (Stage) button2.getScene().getWindow();
-        stage.close();
-    }
+    // public void button2OnAction(ActionEvent e) throws IOException {
+    // Stage stage = (Stage) button2.getScene().getWindow();
+    // stage.close();
+    // }
 
-    public void button1OnAction(ActionEvent e) throws IOException {
-        if (passwordInput.getText().isBlank() == false && usernameInput.getText().isBlank() == false) {
-            message.setText("You tried to login");
-        } else {
-            message.setText("Invalid username or password");
-        }
-    }
+    // public void button1OnAction(ActionEvent e) throws IOException {
+    // if (passwordInput.getText().isBlank() == false &&
+    // usernameInput.getText().isBlank() == false) {
+    // message.setText("You tried to login");
+    // } else {
+    // message.setText("Invalid username or password");
+    // }
+    // }
 
 }
