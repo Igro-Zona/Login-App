be842bd0263d5a3c7a1fd5c010b35b482e7a6e0f
Igro-Zona <tim.bic22@gmail.com>
2025-06-20 19:06:10 +0300

    feat(logic): restored and improved the button functionality

--- src/main/java/com/login/LoginController.java
+++ src/main/java/com/login/LoginController.java
@@ -9,9 +9,11 @@ import javafx.scene.control.Label;
 import javafx.scene.control.PasswordField;
 import javafx.scene.control.SplitPane;
 import javafx.scene.control.TextField;
-import javafx.stage.Stage;
 
 public class LoginController {
+    @FXML
+    private SplitPane splitPane;
+
     @FXML
     private Label loginMessage;
 
@@ -28,47 +30,28 @@ public class LoginController {
     private Button closeButton;
 
     @FXML
-    private SplitPane splitPane;
-
-    // @FXML
-    // private Label message;
-
-    // @FXML
-    // private PasswordField passwordInput;
-
-    // @FXML
-    // private TextField usernameInput;
-
-    // @FXML
-    // private Button button1;
-
-    // @FXML
-    // private Button button2;
-
-    // public void initialize() {
-    // message.setText("Enter your informaion:");
-    // }
-
-    // public void button2OnAction(ActionEvent e) throws IOException {
-    // Stage stage = (Stage) button2.getScene().getWindow();
-    // stage.close();
-    // }
-
-    // public void button1OnAction(ActionEvent e) throws IOException {
-    // if (passwordInput.getText().isBlank() == false &&
-    // usernameInput.getText().isBlank() == false) {
-    // message.setText("You tried to login");
-    // } else {
-    // message.setText("Invalid username or password");
-    // }
-    // }
-
-    @FXML
     public void initialize() {
         SplitPane.Divider divider = splitPane.getDividers().get(0);
         divider.positionProperty().addListener((obs, oldVal, newVal) -> {
             divider.setPosition(0.5);
         });
+    }
+
+    @FXML
+    public void loginButtonOnAction(ActionEvent e) throws IOException {
+        if (usernameField.getText().isBlank() == false &&
+                passwordField.getText().isBlank() == false) {
+            if (usernameField.getText().equals("Timur") && passwordField.getText().equals("12345")) {
+                loginMessage.setText("Succesfull login");
+            } else {
+                loginMessage.setText("Invalid username or password");
+            }
+        }
+    }
+
+    @FXML
+    public void closeButtonOnAction(ActionEvent e) throws IOException {
+        System.exit(0);
     }
 
 }
